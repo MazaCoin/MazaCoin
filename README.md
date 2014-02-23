7624c436468627f7d3dd85d7189d589fac7dae40
maza <maza>
2014-02-22 21:07:06 -0500

    release

---
+++ README.md
@@ -0,0 +1,32 @@
+Mazacoin /(OSX Mavericks Fork)
+=================================
+
+http://www.MazaCoin.org
+https://bitcointalk.org/index.php?topic=267545.0
+
+Copyright (c) 2009-2013 Bitcoin Developers,
+Copyright (c) 2013 Zetacoin Developers,
+Copyright (c) 2014 Mazacoin Developers
+
+What is Zetacoin?
+----------------
+
+Zetacoin is an experimental new digital currency that enables instant payments to
+anyone, anywhere in the world. Zetacoin uses peer-to-peer technology to operate
+with no central authority: managing transactions and issuing money are carried
+out collectively by the network. Zetacoin is also the name of the open source
+software which enables the use of this currency.
+
+For more information, as well as an immediately useable, binary version of
+the Zetacoin client sofware, see https://bitcointalk.org/index.php?topic=267545.0
+
+Note
+-------
+
+This is a personal fork of zetacoin(Dec 23 2013) that works on OSX Mavericks with brew. Use at your own risk.
+
+License
+-------
+
+Zetacoin is released under the terms of the MIT license. See `COPYING` for more
+information or see http://opensource.org/licenses/MIT.
---
+++ src/chainparams.cpp
@@ -0,0 +1,239 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2013 The Zetacoin developers
+// Copyright (c) 2014 The Mazacoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "assert.h"
+
+#include "chainparams.h"
+#include "core.h"
+#include "protocol.h"
+#include "util.h"
+
+//
+// Main network
+//
+
+unsigned int pnSeed[] =
+{
+    0x12345678
+};
+
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        pchMessageStart[0] = 0xf8;
+        pchMessageStart[1] = 0xb5;
+        pchMessageStart[2] = 0x03;
+        pchMessageStart[3] = 0xdf;
+        vAlertPubKey = ParseHex("04f09702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284");
+        nDefaultPort = 12835;
+        nRPCPort = 12832;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
+        nSubsidyHalvingInterval = 241920;
+
+        // Build the genesis block. Note that the output of the genesis coinbase cannot
+        // be spent as it did not originally exist in the database.
+
+        const char* pszTimestamp = "February 5, 2014: The Black Hills are not for sale - 1868 Is The LAW!";
+        CTransaction txNew;
+        txNew.vin.resize(1);
+        txNew.vout.resize(1);
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vout[0].nValue = 5000 * COIN;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime    = 1390747675;
+        genesis.nBits    = 0x1e0ffff0;
+        genesis.nNonce   = 2091390249;
+
+        //// debug print
+        hashGenesisBlock = genesis.GetHash();
+        //while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+        //   if (++genesis.nNonce==0) break;
+        //    hashGenesisBlock = genesis.GetHash();
+        //}
+
+        printf("%s\n", hashGenesisBlock.ToString().c_str());
+        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
+        printf("%x\n", bnProofOfWorkLimit.GetCompact());
+        genesis.print();
+
+
+        assert(hashGenesisBlock == uint256("0x00000c7c73d8ce604178dae13f0fc6ec0be3275614366d44b1b4b5c6e238c60c"));
+        assert(genesis.hashMerkleRoot == uint256("0x62d496378e5834989dd9594cfc168dbb76f84a39bbda18286cddc7d1d1589f4f"));
+
+        vSeeds.push_back(CDNSSeedData("node.mazacoin.org", "node.mazacoin.org"));
+        vSeeds.push_back(CDNSSeedData("node.mazacoin.cf", "node.mazacoin.cf"));
+        vSeeds.push_back(CDNSSeedData("mazacoin.no-ip.org", "mazacoin.no-ip.org"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = 50;
+        base58Prefixes[SCRIPT_ADDRESS] = 9;
+        base58Prefixes[SECRET_KEY] = 224;
+
+        // Convert the pnSeeds array into usable address objects.
+        for (unsigned int i = 0; i < ARRAYLEN(pnSeed); i++)
+        {
+            // It'll only connect to one or two seed nodes because once it connects,
+            // it'll get a pile of addresses with newer timestamps.
+            // Seed nodes are given a random 'last seen time'
+            const int64 nTwoDays = 2 * 24 * 60 * 60;
+            struct in_addr ip;
+            memcpy(&ip, &pnSeed[i], sizeof(ip));
+            CAddress addr(CService(ip, GetDefaultPort()));
+            addr.nTime = GetTime() - GetRand(nTwoDays) - nTwoDays;
+            vFixedSeeds.push_back(addr);
+        }
+    }
+
+    virtual const CBlock& GenesisBlock() const { return genesis; }
+    virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+    virtual const vector<CAddress>& FixedSeeds() const {
+        return vFixedSeeds;
+    }
+protected:
+    CBlock genesis;
+    vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+//
+// Testnet (v3)
+//
+class CTestNetParams : public CMainParams {
+public:
+    CTestNetParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        pchMessageStart[0] = 0x05;
+        pchMessageStart[1] = 0xfe;
+        pchMessageStart[2] = 0xa9;
+        pchMessageStart[3] = 0x01;
+        vAlertPubKey = ParseHex("04303390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a");
+        nDefaultPort = 11835;
+        nRPCPort = 11832;
+        strDataDir = "testnet";
+
+        // Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nTime = 1390747675;
+        genesis.nNonce = 414708675;
+
+
+        //// debug print
+        hashGenesisBlock = genesis.GetHash();
+        while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+            if (++genesis.nNonce==0) break;
+           hashGenesisBlock = genesis.GetHash();
+        }
+
+        printf("%s\n", hashGenesisBlock.ToString().c_str());
+        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
+        genesis.print();
+
+        //assert(hashGenesisBlock == uint256("0x000007717e2e2df52a9ff29b0771901c9c12f5cbb4914cdf0c8047b459bb21d8"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+        // vSeeds.push_back(CDNSSeedData("mazacoin.test", "test.mazacoin.org"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = 88;
+        base58Prefixes[SCRIPT_ADDRESS] = 188;
+        base58Prefixes[SECRET_KEY] = 239;
+
+    }
+    virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+//
+// Regression test
+//
+class CRegTestParams : public CTestNetParams {
+public:
+    CRegTestParams() {
+        pchMessageStart[0] = 0xfa;
+        pchMessageStart[1] = 0x0f;
+        pchMessageStart[2] = 0xa5;
+        pchMessageStart[3] = 0x5a;
+        nSubsidyHalvingInterval = 150;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 1);
+        genesis.nTime = 1390748221;
+        genesis.nBits = 0x207fffff;
+        genesis.nNonce = 3;
+        hashGenesisBlock = genesis.GetHash();
+        nDefaultPort = 11444;
+        strDataDir = "regtest";
+
+        // debug print
+        hashGenesisBlock = genesis.GetHash();
+        while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+           if (++genesis.nNonce==0) break;
+            hashGenesisBlock = genesis.GetHash();
+        }
+
+        printf("%s\n", hashGenesisBlock.ToString().c_str());
+        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
+        genesis.print();
+
+        // assert(hashGenesisBlock == uint256("0x13d8d31dde96874006da503dd2b63fa68c698dc823334359e417aa3a92f80433"));
+
+        vSeeds.clear();  // Regtest mode doesn't have any DNS seeds.
+
+        base58Prefixes[PUBKEY_ADDRESS] = 0;
+        base58Prefixes[SCRIPT_ADDRESS] = 5;
+        base58Prefixes[SECRET_KEY] = 128;
+    }
+
+    virtual bool RequireRPCPassword() const { return false; }
+    virtual Network NetworkID() const { return CChainParams::REGTEST; }
+};
+static CRegTestParams regTestParams;
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+    return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+    switch (network) {
+        case CChainParams::MAIN:
+            pCurrentParams = &mainParams;
+            break;
+        case CChainParams::TESTNET:
+            pCurrentParams = &testNetParams;
+            break;
+        case CChainParams::REGTEST:
+            pCurrentParams = &regTestParams;
+            break;
+        default:
+            assert(false && "Unimplemented network");
+            return;
+    }
+}
+
+bool SelectParamsFromCommandLine() {
+    bool fRegTest = GetBoolArg("-regtest", false);
+    bool fTestNet = GetBoolArg("-testnet", false);
+
+    if (fTestNet && fRegTest) {
+        return false;
+    }
+
+    if (fRegTest) {
+        SelectParams(CChainParams::REGTEST);
+    } else if (fTestNet) {
+        SelectParams(CChainParams::TESTNET);
+    } else {
+        SelectParams(CChainParams::MAIN);
+    }
+    return true;
+}
